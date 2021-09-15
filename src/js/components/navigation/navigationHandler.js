f5ac2f1f565e98a3a353d490cdf182138fbb6bff
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-09-14 23:13:31 -0300

    mobile menu close when click in some link

--- src/js/components/navigation/navigationHandler.js
+++ src/js/components/navigation/navigationHandler.js
@@ -1,7 +1,11 @@
 const navigation = document.querySelector('.navigation');
+const links = navigation.querySelectorAll('.navigation_link')
 
 navigation.querySelector('.navigation_switch').addEventListener('click', e => {
     navigation.classList.toggle('-visible')
+    if(e.target.checked) {
+        links.forEach(link => link.addEventListener('click', e => navigation.classList.remove('-visible')))
+    }
 });
 
 export default navigation;
