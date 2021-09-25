3eea6da4ea6c3e2b530afcc06ea71514b48ffe63
Addson Coutinho <addsoncoutinho@hotmail.com>
2021-09-24 22:56:47 -0300

    feat: add preloader

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import styles from './scss/app.scss'
 import form from './js/components/form/formHandler'
 import navigation from './js/components/navigation/navigationHandler'
+
+window.addEventListener("load", function(event) {
+    const loader = document.querySelector('.preloader');
+    loader.style.opacity = 0;
+    this.setTimeout(() => {loader.remove()} , 500)
+})
